ab0467dacf862a30bdcdcd6c6da9f3a4dced5a52
Mubashirgit1 <mubashir.connect@gmail.com>
2026-03-12 22:56:25 +0000

    add search filter on all columns

--- products/views.py
+++ products/views.py
@@ -1,19 +1,39 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render,get_object_or_404,redirect, reverse
+from django.core.paginator import Paginator
 from .models import Product
-
+from django.db.models import Q
+from django.contrib import messages
 # Create your views here.
 
 def all_products(request):
     """ A view to show all products, including sorting and search queries """
 
     products = Product.objects.all()
+    query = None
+    
+    if request.GET:
+        if 'q' in request.GET:
+            query = request.GET['q']
+            if not query:
+                messages.error(request, "You didn't enter any search criteria!")
+                return redirect(reverse('products'))
+            
+            queries = Q(name__icontains=query) | Q(description__icontains=query) | Q(category__friendly_name__icontains=query) | Q(sku__icontains=query) | Q(tags__icontains=query) | Q(occasion__icontains=query) | Q(recipient__icontains=query) | Q(tags__icontains=query) | Q(occasion__icontains=query)      
+            products = products.filter(queries)
+    
+            # Paginate products - 15 per page
+            paginator = Paginator(products, 15)
+            page_number = request.GET.get('page')
+            page_obj = paginator.get_page(page_number)
+
+
 
     context = {
-        'products': products,
+        'page_obj': page_obj,
+        'products': page_obj.object_list,
     }
 
     return render(request, 'products/products.html', context)
-
 
 def product_detail(request, product_id):
     """ A view to show individual product details """
