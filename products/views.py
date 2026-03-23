1bdef0f023131b5dbb8de4315c2e9e1d37eb1a86
Mubashirgit1 <mubashir.connect@gmail.com>
2026-03-23 15:48:28 +0000

    add related product section

--- products/views.py
+++ products/views.py
@@ -95,8 +95,13 @@ def product_detail(request, product_id):
 
     product = get_object_or_404(Product, pk=product_id)
 
+    related_products = Product.objects.filter(
+        category=product.category
+    ).exclude(id=product.id).order_by('?')[:8]
+
     context = {
         'product': product,
+        'related_products': related_products,
     }
 
     return render(request, 'products/product_detail.html', context)
