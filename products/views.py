9d1123f18acf9eff70a7e88feed48301fd8df2b8
Mubashirgit1 <mubashir.connect@gmail.com>
2026-04-19 21:01:43 +0100

    git add product url, view and template

--- products/views.py
+++ products/views.py
@@ -3,6 +3,7 @@ from django.core.paginator import Paginator
 from .models import Product,Category
 from django.db.models import Q
 from django.contrib import messages
+from .forms import ProductForm
 # Create your views here.
 
 def all_products(request):
@@ -105,3 +106,12 @@ def product_detail(request, product_id):
     }
 
     return render(request, 'products/product_detail.html', context)
+def add_product(request):
+    """ Add a product to the store """
+    form = ProductForm()
+    template = 'products/add_product.html'
+    context = {
+        'form': form,
+    }
+
+    return render(request, template, context)
