2ba9dff4d7fb1457535b8ba13e52f013a594eaed
Mubashirgit1 <mubashir.connect@gmail.com>
2026-04-22 16:50:14 +0100

    fix image update in edit and add admin

--- pickperfect/settings.py
+++ pickperfect/settings.py
@@ -12,10 +12,12 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 from pathlib import Path
 import os
+import os
+import dj_database_url
 
 SETTINGS_DIR = Path(__file__).resolve().parent
-if (SETTINGS_DIR / 'env.py').is_file():
-    from . import env
+if(os.path.isfile('env.py')):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = SETTINGS_DIR.parent
@@ -120,12 +122,17 @@ LOGIN_REDIRECT_URL = '/' #optional -- default is /
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else: 
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
--- products/forms.py
+++ products/forms.py
@@ -1,12 +1,15 @@
 from django import forms
+from .widgets import CustomClearableFileInput
 from .models import Product, Category
 
 
 class ProductForm(forms.ModelForm):
-
     class Meta:
         model = Product
         fields = '__all__'
+        widgets = {
+            'image': CustomClearableFileInput(),
+        }
 
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
