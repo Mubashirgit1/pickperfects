0049d4ef5f2e60fa4c13dd3b053638fe8019546f
Mubashirgit1 <mubashir.connect@gmail.com>
2026-04-25 09:31:29 +0100

    email configuration

--- pickperfect/settings.py
+++ pickperfect/settings.py
@@ -111,7 +111,6 @@ WSGI_APPLICATION = 'pickperfect.wsgi.application'
 MESSAGE_STORAGE = 'django.contrib.messages.storage.session.SessionStorage'
 
 SITE_ID = 1
-EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend' #required for allauth
 ACCOUNT_AUTHENTICATION_METHOD = 'username_email' #required for allauth
 ACCOUNT_EMAIL_REQUIRED = True #required for allauth
 ACCOUNT_EMAIL_VERIFICATION = 'mandatory' #required for allauth
@@ -227,4 +226,14 @@ STRIPE_CURRENCY = 'GBP'
 STRIPE_PUBLIC_KEY = os.getenv('STRIPE_PUBLIC_KEY', '')
 STRIPE_SECRET_KEY = os.getenv('STRIPE_SECRET_KEY', '')
 STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
-DEFAULT_FROM_EMAIL = 'info@pickperfect.com'
+if 'DEVELOPMENT' in os.environ:
+    EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+    DEFAULT_FROM_EMAIL = 'info@pickperfect.com'
+else:
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_TLS = True
+    EMAIL_PORT = 587
+    EMAIL_HOST = 'smtp.gmail.com'
+    EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
+    EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASS')
+    DEFAULT_FROM_EMAIL = os.environ.get('DEFAULT_FROM_EMAIL')
